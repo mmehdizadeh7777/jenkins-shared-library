b6eb56a5bc180f8333c17d2594ab140a8f0dc623
maryam <maryam.mehdizadeh7777@gmail.com>
2023-08-24 11:39:23 +0200

    changes in buildimage

--- vars/buildimage.groovy
+++ vars/buildimage.groovy
@@ -1,8 +1,12 @@
 #!/usr/bin/env groovy
+environment {
+    DOCKERHUB_CREDENTIALS = credentials('dockerhub')
+}
 def call(){
     echo "building and pushing the docker image"
-    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')])
+    //withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')])
     sh "docker build -t mmehdizadeh7777/maven-example:1.1 ."
-    sh "echo $PASS | docker login -u $USER --password-stdin"
+    //sh "echo $PASS | docker login -u $USER --password-stdin"
+    sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
     sh "docker push mmehdizadeh7777/maven-example:1.1"
 }
