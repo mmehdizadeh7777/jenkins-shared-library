7154a5f9bcdf3029eccf9b00cbe72d013d8a58ac
mmehdizadeh7777 <137446885+mmehdizadeh7777@users.noreply.github.com>
2023-08-24 12:31:04 +0200

    Update buildimage.groovy

--- vars/buildimage.groovy
+++ vars/buildimage.groovy
@@ -7,6 +7,6 @@ def call(){
     //withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')])
     sh "docker build -t mmehdizadeh7777/maven-example:1.1 ."
     //sh "echo $PASS | docker login -u $USER --password-stdin"
-    sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+    sh "docker login -u $DOCKERHUB_CREDENTIALS_USR -p $DOCKERHUB_CREDENTIALS_PSW"
     sh "docker push mmehdizadeh7777/maven-example:1.1"
 }
