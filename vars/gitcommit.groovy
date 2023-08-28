13a112675dfe83fa4f4c36f8e002ccf328017016
maryam <maryam.mehdizadeh7777@gmail.com>
2023-08-28 11:51:10 +0200

    change in buildimage

--- vars/gitcommit.groovy
+++ vars/gitcommit.groovy
@@ -5,10 +5,12 @@ def call(String dockerImage){
     withCredentials([usernamePassword(credentialsId: 'github_creds', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
         sh 'git config --global user.email "jenkins@example.com"'
         sh 'git config --global user.name "jenkins"'
+
         sh "git remote set-url origin https://${USER}:${PASS}@github.com/mmehdizadeh7777/MavenBuild.git"
         sh 'git add .'
-        sh 'git commit -m "Jenkins version bump"'
+        sh 'git commit -m "jenkins: version bump"'
         sh 'git push origin HEAD:jenkins-jobs'
+
     }
 
 }
