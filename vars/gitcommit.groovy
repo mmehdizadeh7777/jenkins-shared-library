5525e8a7c8657762cd2d877bcf6dbdd1a898ba2a
maryam <maryam.mehdizadeh7777@gmail.com>
2023-08-28 13:06:22 +0200

    change in buildimage

--- vars/gitcommit.groovy
+++ vars/gitcommit.groovy
@@ -3,7 +3,7 @@
 def call(String dockerImage){
     echo "commiting changes from jenkins into gitHub"
         //withCredentials([usernamePassword(credentialsId: 'github_creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        withCredentials([string(credentialsId: "github-token", variable: 'github-token')]) {
+        withCredentials([String(credentialsId: "github-token", variable: 'github-token')]) {
 
         //def encodedPassword = URLEncoder.encode("$PASS",'UTF-8')
         sh "git remote set-url origin https://${github-token}@github.com/mmehdizadeh7777/MavenBuild.git"
